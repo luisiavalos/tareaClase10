311cec798c3c5a14a331f1d7908282d842ec982c
luisiavalos <130627937+luisiavalos@users.noreply.github.com>
2023-05-27 00:44:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var nombre = "Luisa Fernanda";
+var apellido = "Avalos";
+
+console.log(nombre + " " + apellido);
+
+var n1 = 10;
+var n2 = 20;
+var suma = n1 + n2;
+
+console.log(suma);
+
+var sumaMas2 = suma + 2;
+
+console.log(sumaMas2);
